c7fddc0a44c7da6cd21bf51000cc2fcb0cb85535
ramasai1982 <ramasami.pannirselvame@gmail.com>
2024-05-22 21:50:01 +0200

    PassDao with test and PassRepo

--- src/main/java/com/exalt/producerconsumer/ProducerConsumerApplication.java
+++ src/main/java/com/exalt/producerconsumer/ProducerConsumerApplication.java
@@ -3,17 +3,12 @@ package com.exalt.producerconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.ParseException;
-
 @SpringBootApplication
 public class ProducerConsumerApplication {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args)  {
 		SpringApplication.run(ProducerConsumerApplication.class, args);
-/*		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		long now = new Date().getTime();
-		Pass pass = new Pass(1L,"Rama","Pannir",true,dateFormat.parse("29/10/1982 00:00:00"), dateFormat.format(now), dateFormat.format(now));
-		System.out.println(pass);*/
+
 	}
 
 }
